2d16cb656ee0a627b884a0e36aebfe26ef6e8d64
krasnikovRS <62871275+krasnikovRS@users.noreply.github.com>
2020-07-05 12:30:36 +0300

    init project

---
+++ src/test/java/com/ussidere/pwhelper/PwHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ussidere.pwhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PwHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
